4db9d980d769cc3f64abc5c0a77b70fe00680d84
NoteBuki <seilapolaa@gmail.com>
2023-12-12 20:51:20 -0300

    Primeiro commit no branch main

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Support;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function index(Support $support)
+    {
+        $supports = $support->all();
+
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view ('admin/supports/create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+}
---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,24 @@
+<h1>Lista de Supports</h1>
+
+<a href="{{ route('supports.create') }}">Nova Dúvida</a>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->assunto }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
